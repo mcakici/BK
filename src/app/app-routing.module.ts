e743a2a025bc638035bbe3f9e61eef8519527916
Adamium <mustafa.cakici@adayazilim.com>
2023-07-18 03:40:05 +0300

    Collers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent, title: 'Collers - Landing Page', pathMatch: 'full' },  
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
