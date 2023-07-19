127d1404d15ecc4bfdf6d6037c2b2e7ad97f2b9e
Mustafa Çakıcı <mustafa.cakici@adayazilim.com>
2023-07-20 02:57:55 +0300

    custompage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full', title: 'Collers - Landing Page' },
 
   { path: 'home', component: HomeComponent, title: 'Collers', pathMatch: 'full' },
-  { path: 'products', component: CustompageComponent, title: 'Products - Collers' },
-  { path: 'solutions', component: CustompageComponent, title: 'Solutions - Collers' },
-  { path: 'pricing', component: CustompageComponent, title: 'Pricing - Collers' },
-  { path: 'resources', component: CustompageComponent, title: 'Resources - Collers' },
-  { path: 'login', component: CustompageComponent, title: 'Login - Collers' },
-  { path: 'signup', component: CustompageComponent, title: 'Sign Up - Collers' },
+  { path: 'products', component: CustompageComponent, title: 'Products - Collers', data: {title: 'Products'} },
+  { path: 'solutions', component: CustompageComponent, title: 'Solutions - Collers', data: {title: 'Solutions'} },
+  { path: 'pricing', component: CustompageComponent, title: 'Pricing - Collers', data: {title: 'Pricing'} },
+  { path: 'resources', component: CustompageComponent, title: 'Resources - Collers', data: {title: 'Resources'} },
+  { path: 'login', component: CustompageComponent, title: 'Login - Collers', data: {title: 'Login'} },
+  { path: 'signup', component: CustompageComponent, title: 'Sign Up - Collers', data: {title: 'Signup'} },
   { path: 'quizApp', component: QuizAppComponent, title: 'Quiz App - Collers' },
 
 ];
--- src/app/custompage/custompage.component.ts
+++ src/app/custompage/custompage.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-custompage',
   templateUrl: './custompage.component.html',
-  styleUrls: ['./custompage.component.scss']
+  styleUrls: ['./custompage.component.scss'],
 })
 export class CustompageComponent {
-
+  public data:any;
+  constructor(private activatedroute: ActivatedRoute) {
+    this.activatedroute.data.subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
